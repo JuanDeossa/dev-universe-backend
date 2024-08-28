bdbf5b876c073b64500f1e721636bd140f88b2dc
ricdeo718 <juandeossa.nc@gmail.com>
2024-08-28 09:03:00 -0500

    cors fix

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-// import { envs } from "./config/envs";
+import { envs } from "./config/envs";
 
 export const server = express();
 server.use(express.json()); // acepta json en las peticiones que requeran un body
 server.use(cookieParser());
-server.use(cors());
-// server.use(cors({ credentials: true, origin: envs.FRONTEND_BASE_URL }));
+// server.use(cors());
+server.use(cors({ credentials: true, origin: envs.FRONTEND_BASE_URL }));
 server.use(express.urlencoded({ extended: true })); // acepta urlencoded en las peticiones que requeran un body
